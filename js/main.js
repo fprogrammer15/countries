d68d62b22f45113f13641971394f719c3eaa2ef3
dev <haydaraliyevasevdo26@gmail.com>
2026-01-08 13:17:41 +0500

    Add dark mode to JS

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const elDarkModeButton = document.querySelector('.site-header-theme-toggler');
+
+elDarkModeButton.addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+});
